70e52d3bade8c4286d45f512e015b4f5e92ce297
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-03 09:35:07 +0300

    add methods

--- src/entities/expression.py
+++ src/entities/expression.py
@@ -5,6 +5,14 @@ class Expression:
     ) -> None:
         self._raw_expression = raw_expression
         self._tokens = []
+        self._postfix_notation = None
+        self._value = None
+
+    def __str__(self) -> str:
+        raw_expression = f"raw expression: {self._raw_expression}"
+        tokens = f"tokens: {self._tokens}"
+        postfix = f"postfix notation: {self._postfix_notation}"
+        return f"{raw_expression}\n{tokens}\n{postfix}"
 
     def raw_expression(self) -> str:
         return self._raw_expression
@@ -17,4 +25,18 @@ class Expression:
             tokens: list
     ):
         self._tokens = tokens
+
+    def set_postfix_notation(
+            self,
+            notation: list
+    ):
+        self._postfix_notation = notation
     
+    def postfix(self) -> list:
+        return self._postfix_notation
+    
+    def set_value(
+            self,
+            value
+    ):
+        self._value = value
