3fe9033b60f2dc71b1f94bd714ebab5e770d95cb
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-03 09:34:48 +0300

    add postfix notation evaluation

--- src/services/calculator_service.py
+++ src/services/calculator_service.py
@@ -15,6 +15,24 @@ class CalculatorService:
             self,
             expression: Expression
     ):
-        self._shunting_yard_service.run(expression)
+        expression = self._shunting_yard_service.run(expression)
+        result = self._evaluate_postfix_notation(expression.postfix())
+        return result
+
+    def _evaluate_postfix_notation(
+            self,
+            postfix_notation: list
+    ) -> int:
+        stack = []
+        for token in postfix_notation:
+            if token.isdigit():
+                stack.append(token)
+            else:
+                val_1 = stack.pop()
+                val_2 = stack.pop()            
+                stack.append(str(eval(val_2 + token + val_1)))
+
+        result = float(stack.pop())
+        return result
 
 calculator_service = CalculatorService()
