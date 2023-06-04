225edb305fa0a04c696289b6af78e472636e7e17
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-04 20:00:30 +0300

    change ' to "

--- tasks.py
+++ tasks.py
@@ -2,23 +2,23 @@ from invoke import task
 
 @task
 def start(ctx):
-    ctx.run('python3 src/calculator.py')
+    ctx.run("python3 src/calculator.py")
 
 @task
 def test(ctx):
-    ctx.run('pytest src')
+    ctx.run("pytest src")
 
 @task
 def coverage(ctx):
-    ctx.run('coverage run --branch -m pytest src')
+    ctx.run("coverage run --branch -m pytest src")
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run('coverage html')
+    ctx.run("coverage html")
 
 @task
 def lint(ctx):
-    ctx.run('pylint src')
+    ctx.run("pylint src")
 
 @task
 def format(ctx):
