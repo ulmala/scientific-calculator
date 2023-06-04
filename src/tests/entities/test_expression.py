fa1da77be77f37f6be1123d380629d54af31ded2
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-04 18:49:47 +0300

    add tests

--- src/tests/entities/test_expression.py
+++ src/tests/entities/test_expression.py
@@ -20,3 +20,23 @@ class TestExpression(unittest.TestCase):
         tokens = ["1", "+", "1"]
         self.expression.set_tokens(tokens)
         self.assertEqual(tokens, self.expression.tokens())
+
+    def test_postfix_returns_correct_value(self):
+        self.expression.set_postfix(["+34"])
+        self.assertEqual(["+34"], self.expression.postfix())
+
+    def test_set_postfix_sets_value_to_class_variable(self):
+        self.assertEqual(self.expression._postfix_notation, None)
+        self.expression.set_postfix(["+34"])
+        self.assertEqual(self.expression._postfix_notation, ["+34"])
+
+    def test_set_value_sets_value_to_class_varibale(self):
+        self.assertEqual(self.expression._value, None)
+        self.expression.set_value(2)
+        self.assertEqual(2, self.expression._value)
+
+    def test_str_representation(self):
+        self.expression._tokens = ["1", "+", "1"]
+        self.expression._postfix_notation = ["+", "1", "1"]
+        correct_str =  f"raw expression: 1+1\ntokens: ['1', '+', '1']\npostfix notation: ['+', '1', '1']"
+        self.assertEqual(correct_str, str(self.expression))
--- src/tests/entities/test_output_queue.py
+++ src/tests/entities/test_output_queue.py
@@ -14,3 +14,9 @@ class TestOutputQueue(unittest.TestCase):
     def test_get_returns_correct_value(self):
         self.output_queue.put("+")
         self.assertEqual("+", self.output_queue.get())
+
+    def test_as_list_returns_correct_value(self):
+        self.output_queue.put("+")
+        self.output_queue.put("-")
+        queue_list = self.output_queue.as_list()
+        self.assertEqual(["+", "-"], queue_list)
