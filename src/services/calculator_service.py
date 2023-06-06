0adeb5633f3d1fedc5af7e3073c8942d34fc4f0c
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-06 20:01:22 +0300

    add support for funcs

--- src/services/calculator_service.py
+++ src/services/calculator_service.py
@@ -1,9 +1,15 @@
+import math
 from ast import literal_eval
 from entities.expression import Expression
 from services.shunting_yard_service import (
     shunting_yard_service as default_shunting_yard_service
 )
 
+from constants import (
+    SUPPORTED_FUNCTIONS,
+    ONE_PARAMETER_FUNCTIONS,
+    TWO_PARAMETER_FUNCTIONS
+)
 
 class CalculatorService:
     """
@@ -41,6 +47,110 @@ class CalculatorService:
         result = self._evaluate_postfix_notation(expression.postfix())
         return result
 
+    def _calculate_basic_operation(
+            self,
+            token: str,
+            stack: list
+    ) -> str:
+        operand_1 = stack.pop()
+        operand_2 = stack.pop()
+        if token == "^": token = "**"
+        return str(eval(operand_2 + token + operand_1)) # TODO: do this using ast; https://stackoverflow.com/questions/2371436/evaluating-a-mathematical-expression-in-a-string
+
+    def _calculate_one_parameter_function(
+            self,
+            token: str,
+            stack: list
+    ) -> str:
+        """
+        Calculates value of one parameter functions.
+        e.g. sin(90)
+
+        Args:
+            token (str): token
+            stack (list): stack 
+
+        Returns:
+            str: value of function as string
+        """
+        operand = stack.pop()
+        value = SUPPORTED_FUNCTIONS[token](float(operand))
+        return str(value)
+
+    def _calculate_two_parameter_function(
+            self,
+            token: str,
+            stack: list
+    ) -> str:
+        """
+        Calculates value of function with two parameters.
+        e.g. max(2,3)
+
+        Args:
+            token (str): token
+            stack (list): stack
+
+        Returns:
+            str: value of function as string
+        """
+        operand_2 = stack.pop()
+        operand_1 = stack.pop()
+        result = SUPPORTED_FUNCTIONS[token](float(operand_1), float(operand_2))
+        return str(result)
+
+    def _is_one_parameter_function(
+            self,
+            token: str
+    ) -> bool:
+        """
+        Checks if the function is one parameter function.
+
+        Args:
+            token (str): token to be validated
+
+        Returns:
+            bool: True if one parameter function, else False
+        """
+        if token in ONE_PARAMETER_FUNCTIONS:
+            return True
+        return False
+    
+    def _is_two_parameter_function(
+            self,
+            token: str
+    ) -> bool:
+        """
+        Checks if the function is two parameter function.
+
+        Args:
+            token (str): token to be validated
+
+        Returns:
+            bool: True if two parameter function, else False
+        """
+        if token in TWO_PARAMETER_FUNCTIONS:
+            return True
+        return False
+    
+    def _is_number(
+            self,
+            token: str
+    ) -> bool:
+        """
+        Checks if given token is number
+
+        Args:
+            token (str): token to be validated
+
+        Returns:
+            bool: True if number, else False
+        """
+        try:
+            float(token)
+            return True
+        except:
+            return False
+
     def _evaluate_postfix_notation(
             self,
             postfix_notation: list
@@ -56,12 +166,20 @@ class CalculatorService:
         """
         stack = []
         for token in postfix_notation:
-            if token.isdigit():
+            if self._is_number(token):
                 stack.append(token)
+            elif self._is_one_parameter_function(token):
+                stack.append(
+                    self._calculate_one_parameter_function(token, stack)
+                )
+            elif self._is_two_parameter_function(token):
+                stack.append(
+                    self._calculate_two_parameter_function(token, stack)
+                )
             else:
-                val_1 = stack.pop()
-                val_2 = stack.pop()
-                stack.append(str(eval(val_2 + token + val_1))) # TODO: do this using ast; https://stackoverflow.com/questions/2371436/evaluating-a-mathematical-expression-in-a-string
+                stack.append(
+                    self._calculate_basic_operation(token, stack)
+                )
 
         result = float(stack.pop())
         return result
