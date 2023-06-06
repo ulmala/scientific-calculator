376c0fed00ce23e3352103e6d8f1648d397cd7fb
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-06 19:47:28 +0300

    add functions

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,22 @@
+import math
+
+
 OPERATOR_PRECEDENCE = {
     "(": 0,
     "-": 1,
     "+": 1,
     "/": 2,
     "*": 2,
+    "^": 3
 }
 
-OPERATORS = ["+", "-", "*", "/"]
+OPERATORS = ["+", "-", "*", "/", "^"]
 LEFT_ASSOCIATIVE_OPERATORS = ["+", "-", "*", "/"]
+
+SUPPORTED_FUNCTIONS = {
+    "sin": math.sin,
+    "max": max
+}
+
+ONE_PARAMETER_FUNCTIONS = ["sin"]
+TWO_PARAMETER_FUNCTIONS = ["max"]
