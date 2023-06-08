30c0ef150f5209edf0b606e9b8cc72b2f340c70a
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-08 21:22:07 +0300

    add tests

--- src/tests/services/test_validation_service.py
+++ src/tests/services/test_validation_service.py
@@ -2,67 +2,81 @@ import unittest
 from unittest.mock import MagicMock
 from entities.expression import Expression
 from services.validation_service import ValidationService
+from services.validation_service import NotValidExpression
 
 
 class TestValidationService(unittest.TestCase):
     def setUp(self):
         self.validation_service = ValidationService()
 
-    def test_expression_starts_with_digit(self):
+    def test_expression_starts_with_number(self):
         valid_expression = Expression("2+1")
         invalid_expression = Expression("+1")
         self.assertTrue(
-            self.validation_service.expression_starts_with_digit(valid_expression))
+            self.validation_service._expression_starts_with_number(valid_expression))
         self.assertFalse(
-            self.validation_service.expression_starts_with_digit(invalid_expression))
+            self.validation_service._expression_starts_with_number(invalid_expression))
 
     def test_expression_starts_with_left_paranthesis(self):
         valid_expression = Expression("(1+1)")
         invalid_expression = Expression("1+1")
         self.assertTrue(
-            self.validation_service.expression_starts_with_left_paranthesis(valid_expression))
-        self.assertFalse(self.validation_service.expression_starts_with_left_paranthesis(
+            self.validation_service._expression_starts_with_left_paranthesis(valid_expression))
+        self.assertFalse(self.validation_service._expression_starts_with_left_paranthesis(
             invalid_expression))
 
-    @unittest.skip(reason="not implemented")
-    def test__expression_starts_with_valid_token(self):
-        valid_expressions = [
+    def test_validate_expression_does_not_raise_error_if_one_of_the_validations_is_passed(self):
+        expressions = [
+            Expression("(1+1)"),
             Expression("1+1"),
-            Expression("(1+1)")
+            Expression("sin(90)"),
+            Expression("+1+1"),
+            Expression("-1+1"),
         ]
-        invalid_expression = Expression("+3-1")
-        for valid_expression in valid_expressions:
-            self.assertTrue(
-                self.validation_service._expression_starts_with_valid_token(valid_expression))
-        self.assertFalse(
-            self.validation_service._expression_starts_with_valid_token(invalid_expression))
+        for expression in expressions:
+            self.assertIsNone(
+                self.validation_service.validate_expression(expression))
 
-    def test_validate_expression_returns_true_if_all_validations_passed(self):
-        expression = Expression("(1+1)")
-        self.assertTrue(
-            self.validation_service.validate_expression(expression))
+    def test_validate_expression_raises_error_if_all_validations_fail(self):
+        expressions = [
+            Expression("?1+1"),
+            Expression(")1+1")
+        ]
+        for expression in expressions:
+            with self.assertRaises(NotValidExpression):
+                self.validation_service.validate_expression(expression)
 
-    @unittest.skip(reason="not implemented")
-    def test_validate_expression_returns_false_if_any_validation_fails(self):
-        expression = Expression("+3-1")
-        self.assertFalse(
-            self.validation_service.validate_expression(expression))
-
-    def test__is_operator_returns_true_if_operator(self):
+    def test_is_operator_returns_true_if_operator(self):
         self.assertTrue(self.validation_service.is_operator("-"))
         self.assertTrue(self.validation_service.is_operator("+"))
         self.assertTrue(self.validation_service.is_operator("/"))
         self.assertTrue(self.validation_service.is_operator("*"))
 
-    def test__is_operator_returns_false_if_not_operator(self):
+    def test_is_operator_returns_false_if_not_operator(self):
         self.assertFalse(self.validation_service.is_operator("2"))
         self.assertFalse(self.validation_service.is_operator("200"))
 
-    def test__is_left_associative_returns_true_if_operator_left_associative(self):
+    def test_is_left_associative_returns_true_if_operator_left_associative(self):
         self.assertTrue(self.validation_service.is_left_associative("-"))
         self.assertTrue(self.validation_service.is_left_associative("+"))
         self.assertTrue(self.validation_service.is_left_associative("/"))
         self.assertTrue(self.validation_service.is_left_associative("*"))
 
-    def test__is_left_associative_returns_false_if_operator_not_left_associative(self):
+    def test_is_left_associative_returns_false_if_operator_not_left_associative(self):
         self.assertFalse(self.validation_service.is_left_associative("^"))
+
+    def test__matching_parantheses_does_not_raise_error_if_matching_parantheses(self):
+        expression = Expression("((1+2)*3)")
+        self.assertIsNone(
+            self.validation_service._matching_parantheses(expression))
+        expression = Expression("(1+1)")
+        self.assertIsNone(
+            self.validation_service._matching_parantheses(expression))
+
+    def test__matching_parantheses_raises_error_if_mismatching_parantheses(self):
+        expression = Expression("((1+1)")
+        with self.assertRaises(NotValidExpression):
+            self.validation_service._matching_parantheses(expression)
+        expression = Expression("1+1)")
+        with self.assertRaises(NotValidExpression):
+            self.validation_service._matching_parantheses(expression)
