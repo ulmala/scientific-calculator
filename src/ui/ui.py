8f096904fa93b6e1113e597063ce9fe7aae44cde
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-18 20:20:20 +0300

    reafactor

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,36 +1,35 @@
 from services.calculator_service import calculator_service
-from services.parser_service import parser_service
 from services.validation_service import validation_service
 from services.validation_service import NotValidExpression
-from entities.expression import Expression
 
 
 class UI:
     def start(self):
         while True:
-            for _ in range(2):
-                print("\n")
+            for _ in range(100): print("#", end="")
+            print()
             user_input = input(
-                "Type 'var' to declare new variable or 'exp' to submit expression: ")
+                "Type 'var' to declare new variable or 'exp' to submit expression: "
+            )
+
             if user_input == "exp":
                 user_expression = input("Type your equation here: ")
-                expression = Expression(user_expression)
                 try:
-                    result = calculator_service.solve(expression)
+                    expression = calculator_service.solve(user_expression)
                 except NotValidExpression as e:
                     print(f"*!*!*!*!*!*! {e} *!*!*!*!*!*!")
                     continue
 
-                print(f"{' '.join(expression.tokens)} =", result)
+                #print(expression)
 
                 user_input = input(
-                    "Do you want to save the result to variable? Press enter to continue, else write variable name: "
+                    "Enter variable name to store the result into variable (else press Enter): "
                 )
                 if user_input == "":
                     continue
                 calculator_service.add_variable(
                     variable_name=user_input,
-                    variable_value=str(result)
+                    variable_value=str(expression.value)
                 )
 
             if user_input == "var":
@@ -44,6 +43,4 @@ class UI:
                     continue
                 calculator_service.add_variable(
                     variable_name=var_name, variable_value=var_value)
-                print(calculator_service.get_variables_as_string())
-
-            input("\nPress any key to continue")
+            print(calculator_service.list_variables())
