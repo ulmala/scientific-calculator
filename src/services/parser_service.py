4027ec2e204e5cfa3a83b396424f6ac8d0c874b4
Olli Ulmala <ulmala.olli@hotmail.com>
2023-06-20 20:45:07 +0300

    refactor

--- src/services/parser_service.py
+++ src/services/parser_service.py
@@ -44,6 +44,36 @@ class ParserService:
     ):
         expression.raw_expression = expression.raw_expression.replace(" ", "")
         return expression
+    
+    def _add_leading_zero_if_starting_with_minus(
+            self,
+            expression: Expression
+    ) -> Expression:
+        """
+        If user's expression starts with '-' this methdo will add
+        leading zero to the expression. This way the Shuting Yard
+        algorithm will handle expression correctly
+
+        Args:
+            expression (Expression): expression to be checked
+
+        Returns:
+            Expression: modified expression
+        """
+        if expression.raw_expression[0] == "-":
+            expression.raw_expression = "0" + expression.raw_expression
+        return expression
+    
+    def _get_tokens(
+            self,
+            expression: Expression,
+            variables: dict
+    ) -> Expression:
+        escaped_funcs_and_vars = self._get_escaped_funcs_and_vars(variables)
+        pattern = r"(\d+(?:\.\d+)?|\+|\-|\*|\^|\/|\(|\)|\,|" + \
+            "|".join(escaped_funcs_and_vars) + ")"
+        tokens = re.findall(pattern, expression.raw_expression)
+        return tokens
 
     def parse_to_tokens(
             self,
@@ -51,22 +81,26 @@ class ParserService:
             variables: dict
     ) -> Expression:
         """
-        Parse raw expression into list of tokens. Updates expression 
-        object and returns it.
+        This is the 'main' function when parsings expression to tokens.
+        Executes following:
+        - removes whitespaces
+        - adds leading zero to the epxression if starting with zero
+        - parse raw epxression in to list of tokens
+        - validates that no tokens were dropped in parsing
+        - converts all varibales into numeric values
+        - sets tokens to the Expression object
 
         Args:
-            expression (Expression): expression object with no tokens
+            expression (Expression): Expression object with no tokens
 
         Returns:
-            Expression: expression object with tokens
+            Expression: Expression object with tokens
         """
         expression = self._remove_whitespaces(expression)
-        escaped_funcs_and_vars = self._get_escaped_funcs_and_vars(variables)
-        pattern = r"(\d+(?:\.\d+)?|\+|\-|\*|\^|\/|\(|\)|\,|" + \
-            "|".join(escaped_funcs_and_vars) + ")"
-        tokens = re.findall(pattern, expression.raw_expression)
-        tokens = self._convert_variables_to_values(tokens, variables)
+        expression = self._add_leading_zero_if_starting_with_minus(expression)
+        tokens = self._get_tokens(expression, variables)
         self._validation_service.check_if_tokens_are_not_dropped(tokens, expression)
+        tokens = self._convert_variables_to_values(tokens, variables)
         expression.tokens = tokens
         return expression
 
